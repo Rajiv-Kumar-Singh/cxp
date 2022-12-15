d517f9cdf218fae22ca9be3c8929cc0fe42e33f0
Rajiv-Kumar-Singh <rv488448@gmail.com>
2022-12-15 15:38:26 +0530

    Fix syntax highlighting

--- pages/htmlSphere.vue
+++ pages/htmlSphere.vue
@@ -6,37 +6,37 @@
 </template>
 
 <script>
-import SourceCode from "../components/SourceCode.vue";
-import Code from "~/components/Code.vue";
+import SourceCode from '../components/SourceCode.vue';
+import Code from '~/components/Code.vue';
 
 // import thumbnails
-import threeDSphere from "~/assets/thumbnails/html-text-sphere.jpg";
+import threeDSphere from '~/assets/thumbnails/html-text-sphere.jpg';
 
 // import code file
-import codes from "~/assets/codes/htmlSphere.js";
+import codes from '~/assets/codes/htmlSphere.js';
 
 export default {
-  name: "HtmlSphere",
+  name: 'HtmlSphere',
   data() {
     return {
       content: {
         title:
-          "Create 3D Rotating Text Sphere Using HTML, CSS, JavaScript and TagCloud.js",
+          'Create 3D Rotating Text Sphere Using HTML, CSS, JavaScript and TagCloud.js',
         image: threeDSphere,
         description:
-          "Hello Developers, In this blog you will learn to create this awesome 3D rotating text sphere using HTML, CSS, JavaScript and a package named TagCloud.js.",
+          'Hello Developers, In this blog you will learn to create this awesome 3D rotating text sphere using HTML, CSS, JavaScript and a package named TagCloud.js.',
         info: [
           {
-            text: "Checkout the source code below and take reference from video to create this rotating text cloud.",
+            text: 'Checkout the source code below and take reference from video to create this rotating text cloud.',
           },
           {
-            text: "Checkout TagCloud.js Here : https://www.npmjs.com/package/TagCloud",
+            text: 'Checkout TagCloud.js Here : https://www.npmjs.com/package/TagCloud',
           },
         ],
-        youtubeVideo: "https://www.youtube.com/embed/Y4Jl1Ql9qFI",
+        youtubeVideo: 'https://www.youtube.com/embed/Y4Jl1Ql9qFI',
       },
       codeContent: {
-        htmlSyntax: false,
+        htmlSyntax: true,
         htmlCode: codes.htmlCode,
         cssCode: codes.cssCode,
       },
