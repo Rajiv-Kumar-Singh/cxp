b48f33ef3930a4bb1758e78af1ee6ead059c3333
Rajiv-Kumar-Singh <rv488448@gmail.com>
2022-12-17 22:47:36 +0530

    lazy load youtube videos

--- components/MainCarousel.vue
+++ components/MainCarousel.vue
@@ -26,6 +26,7 @@
                 title="YouTube video player"
                 frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                loading="lazy"
                 allowfullscreen
               ></iframe>
             </div>
